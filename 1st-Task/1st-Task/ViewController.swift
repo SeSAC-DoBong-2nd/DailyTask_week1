657ce83e4e6743f912b075f0f58c12eb410162e6
ParkSY0919 <qhr498@gmail.com>
2024-12-25 19:51:15 +0900

    [Setting/#1] setup 1st-Task project

---
+++ 1st-Task/1st-Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1st-Task
+//
+//  Created by 박신영 on 12/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

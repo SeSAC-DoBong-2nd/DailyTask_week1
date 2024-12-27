4952a1073e2da4091ca0df1f8f248d00020953e4
ParkSY0919 <qhr498@gmail.com>
2024-12-27 12:47:55 +0900

    [Add/#7] add MainHomeTabViewController

---
+++ 1st-Task/1st-Task/MainHomeTabViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainHomeTabViewController.swift
+//  1st-Task_MovieProject
+//
+//  Created by 박신영 on 12/27/24.
+//
+
+import UIKit
+
+class MainHomeTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}

346b140113bbf940e437a5df914595d41e84363b
ParkSY0919 <qhr498@gmail.com>
2024-12-28 01:53:40 +0900

    [Add/#9] add NeologismFinder Related Files

---
+++ 1st-Task/1st-Task/NeologismFinderViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NeologismFinderViewController.swift
+//  1st-Task_MovieProject
+//
+//  Created by 박신영 on 12/28/24.
+//
+
+import UIKit
+
+class NeologismFinderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

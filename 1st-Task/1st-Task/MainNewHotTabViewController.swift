0f34c805162fb867ef0a3e1fec66e19c4ad7d586
ParkSY0919 <qhr498@gmail.com>
2024-12-27 16:56:07 +0900

    [Fix/#7] 두 번째 탭바 구현 코드 수정

--- 1st-Task/1st-Task/MainNewHotTabViewController.swift
+++ 1st-Task/1st-Task/MainNewHotTabViewController.swift
@@ -42,9 +42,13 @@ class MainNewHotTabViewController: UIViewController {
         }
         
         let titleCommentsArr = ["이런! 찾으시는 작품이 없습니다.", "이런런! 배가 고픕니다!", "허걱! 지각입니다!"]
-        let subtitleCommentsArr = ["다른 영화, 시리즈, 배우, 감독 또는 장르를 검색해 보세요.", "한식, 양식, 중식, 일식 등 메뉴를 검색해 보세요.", "버스, 택시, 지하철 등 이동할 대중교통을 검색해 보세요."]
+        let subtitleCommentsDic: [String: String] = [
+            "이런! 찾으시는 작품이 없습니다.": "다른 영화, 시리즈, 배우, 감독 또는 장르를 검색해 보세요.",
+            "이런런! 배가 고픕니다!": "한식, 양식, 중식, 일식 등 메뉴를 검색해 보세요.",
+            "허걱! 지각입니다!": "버스, 택시, 지하철 등 이동할 대중교통을 검색해 보세요."
+        ]
         
         titleCommentLabel.text = titleCommentsArr.randomElement()
-        subtitleCommentLabel.text = subtitleCommentsArr.randomElement()
+        subtitleCommentLabel.text = subtitleCommentsDic[titleCommentLabel.text ?? ""]
     }
 }
